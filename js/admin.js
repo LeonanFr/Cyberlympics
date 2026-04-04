be425ebdb05622b771b0388c6a3c20459ef983ff
Leonan <pedroleonan85@gmail.com>
2026-04-04 03:08:53 -0300

    fix: correção da rota de start tournament

--- js/admin.js
+++ js/admin.js
@@ -640,7 +640,7 @@
         btn.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> Processando...';
 
         try {
-            const response = await fetch(`${ORCHESTRATOR_URL}/api/admin/start`, {
+            const response = await fetch(`${ORCHESTRATOR_URL}/api/admin/tournament/start`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
